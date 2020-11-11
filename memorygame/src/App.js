4a10210aad31f0e9cff6891708dc9e8d55f4555e
reng <reng412@gmail.com>
2020-11-10 18:02:31 -0800

    Add comments to app.js

--- memorygame/src/App.js
+++ memorygame/src/App.js
@@ -7,6 +7,8 @@ import albums from "./albums.json";
 
 class App extends Component {
     // Setting this.state.albums to the albums json array
+    // this.state.clicked is a set that keeps track of what albums have been clicked
+    // this.state.highScore keeps track of the high score
     state = {
         albums: albums,
         clicked: new Set(),
@@ -14,17 +16,19 @@ class App extends Component {
     };
 
     validateClick = id => {
-        // Filter this.state.albums for albums with an id not equal to the id being removed
+        // Finds the album with specific id in this.state.albums
         const selectAlbum = this.state.albums.find(album => album.id === id);
-        // Set this.state.friends equal to the new friends array
+        // Set this.state.clicked set includes the selected album, end game since it has been selected before
         if (this.state.clicked.has(selectAlbum)) {
-            // End game
+            // End game and set this.state to include a new set that is empty to start a new game
             this.setState(state => ({
                 ...state,
                 clicked: new Set(),
                 // Add logic for clicked correct/incorrect
             }))
         } else {
+            // If this.state.clicked doesn't include the slected album, add the album to the clicked set 
+            // Update high score to reflect either the number of correct albums selected or the existing high score, whichever is larger
             this.setState(state => ({
                 ...state,
                 clicked: state.clicked.add(selectAlbum),
@@ -43,7 +47,6 @@ class App extends Component {
                 <span>Top Score: {this.state.highScore}</span>
                 {this.state.albums.map(album => (
                     <AlbumImg
-                        // removeFriend={this.removeFriend}
                         key={album.id}
                         name={album.name}
                         image={album.image}
